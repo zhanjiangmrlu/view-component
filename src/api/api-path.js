efe10772e14235befb5a746f04744565b659ab1d
lufengkai <zhanjiangmrlu@163.com>
2019-04-28 23:15:27 +0800

    feat: 添加请求axios处理

---
+++ src/api/api-path.js
@@ -0,0 +1,8 @@
+/**
+ * 统一API接口
+ */
+const apiPath = {
+  name: `https://api.myjson.com/bins/1ba9ii`
+}
+
+export default apiPath
--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import { post, fetch, patch, put } from './api/http'
 
 Vue.use(ElementUI)
 
+Vue.prototype.$post = post
+Vue.prototype.$fetch = fetch
+Vue.prototype.$patch = patch
+Vue.prototype.$put = put
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
